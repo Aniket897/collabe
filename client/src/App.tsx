cbc2cf689186fbdccc406b4d7bf41ffab49796f1
Aniket Kangane <aniketbkangane9637@gmail.com>
2024-08-02 14:29:59 +0530

    404 route added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Room from "./Pages/Room";
 import { Toaster } from "sonner";
@@ -7,10 +7,11 @@ const App = () => {
   // all are protected routes because auth is handled in authContext
   return (
     <>
-      <Toaster richColors position="bottom-right" />
+      <Toaster richColors closeButton position="bottom-right" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/room" element={<Room />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     </>
   );
